2e591cfc67bea9aa8800b242ab5e26af2f580c0b
“ryanb4” <ryan4@live.unc.edu>
2021-10-06 14:21:09 -0400

    sumToString

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,8 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+    let sum = a + b;
+    return a + " + " + b + " = " + sum;
 }
 
 
