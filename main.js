d2e9022311e137ce53624927f17be335addb2df5
“ryanb4” <ryan4@live.unc.edu>
2021-11-03 16:50:58 -0400

    curry

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ import { allCarStats, getAvgMpgByYearAndHybrid, getMakerHybrids, moreStats } fro
 import { searchByYear, searchHighPower, searchName } from "./src/medium/medium_3.js";
 import {countArray, maxAndMin, sumToString} from "./src/mild/mild_1.js";  
 import { identifyArray, identifyVariable, removeKey, removeKeyNonDestructive, removeKeys } from "./src/mild/mild_2.js";
-import { repeat, repeatDemo } from "./src/spicy/spicy_9.js";
+import { multiplyBy, repeat, repeatDemo } from "./src/spicy/spicy_9.js";
   
 
 
 
 
 
-repeatDemo();
+console.log(multiplyBy(3)(5));
