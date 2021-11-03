d5d34f3c6dfd352c47e87fe7fa0e950b1480d047
“ryanb4” <ryan4@live.unc.edu>
2021-11-03 17:25:56 -0400

    an even odd some

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ import { allCarStats, getAvgMpgByYearAndHybrid, getMakerHybrids, moreStats } fro
 import { searchByYear, searchHighPower, searchName } from "./src/medium/medium_3.js";
 import {countArray, maxAndMin, sumToString} from "./src/mild/mild_1.js";  
 import { identifyArray, identifyVariable, removeKey, removeKeyNonDestructive, removeKeys } from "./src/mild/mild_2.js";
-import { everyEven, filter, multiplyBy, repeat, repeatDemo, someEven, tenTimes, tenTimesFifty } from "./src/spicy/spicy_9.js";
+import { allEvensAreOdd, anEvenIsOdd, everyEven, filter, multiplyBy, repeat, repeatDemo, someEven, tenTimes, tenTimesFifty } from "./src/spicy/spicy_9.js";
   
 
 
-console.log(filter(['spectacle', 'pretend', 'orange', 'melt'], x => x[2] === 'e'));
+console.log(allEvensAreOdd([4, 5, 6, 7, 8, 9, 7]));
+console.log(anEvenIsOdd([4, 5, 6, 7, 8, 9, 7]));
