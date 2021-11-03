3bd7d49dce242b04c9361435e9df3357f04a23ff
“ryanb4” <ryan4@live.unc.edu>
2021-11-03 16:57:39 -0400

    tentimes

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ import { allCarStats, getAvgMpgByYearAndHybrid, getMakerHybrids, moreStats } fro
 import { searchByYear, searchHighPower, searchName } from "./src/medium/medium_3.js";
 import {countArray, maxAndMin, sumToString} from "./src/mild/mild_1.js";  
 import { identifyArray, identifyVariable, removeKey, removeKeyNonDestructive, removeKeys } from "./src/mild/mild_2.js";
-import { multiplyBy, repeat, repeatDemo } from "./src/spicy/spicy_9.js";
+import { multiplyBy, repeat, repeatDemo, tenTimes } from "./src/spicy/spicy_9.js";
   
 
 
 
 
 
-console.log(multiplyBy(3)(5));
+console.log(tenTimes(37));
--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -49,13 +49,11 @@ export const multiplyBy = (num1) => {
 };
 
 
-
-
 /**
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
